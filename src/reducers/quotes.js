b72d5799c13883a94baf191ca88f3c4b94ffa037
sbigelo <sean.bigelow1@gmail.com>
2021-08-18 15:11:55 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,37 @@
+
+
 export default (state = [], action) => {
-  return state;
+  
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return state.concat(action.quote)
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+    case  "DOWNVOTE_QUOTE":
+      return state.map(quote => {
+        if(quote.id !== action.quoteId || quote.votes === 0) {
+          return quote
+        } else {
+          return {
+            ...quote,
+            votes: quote.votes - 1
+          }
+        }
+      })
+    case "UPVOTE_QUOTE":
+      return state.map(quote => {
+        if (quote.id !== action.quoteId) {
+          return quote
+        } else {
+          return {
+            ...quote,
+            votes: quote.votes + 1
+          }
+        }
+      })
+       
+  default:
+      return state;
+    }
+  
 }
